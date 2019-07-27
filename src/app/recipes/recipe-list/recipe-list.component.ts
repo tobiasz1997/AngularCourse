6d17a79d4f4232b48293793427240ce8c83b57ff
Grzegorz <grzegorz.tobiasz97@gmail.com>
2019-07-27 23:57:47 +0200

    created navbar and components

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pierog', 'Description of Recipes', 'https://www.mniammniam.com/obrazki/u_obrazki/n17511.jpg'),
+    new Recipe('Pierog', 'Description of Recipes', 'https://www.mniammniam.com/obrazki/u_obrazki/n17511.jpg'),
+    new Recipe('Pierog', 'Description of Recipes', 'https://www.mniammniam.com/obrazki/u_obrazki/n17511.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
