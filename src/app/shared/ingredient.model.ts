45e2294884163c039c6cc19b37ba6e0b21a687f7
Grzegorz <grzegorz.tobiasz97@gmail.com>
2019-07-28 13:46:19 +0200

    finished 1 part of the course 'the basics'

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+  constructor (public name: string, public amount: number) {}
+}
+
+  // this is the same as below
+  // public name: string;
+  // public amount: number;
+  //
+  // constructor (name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
